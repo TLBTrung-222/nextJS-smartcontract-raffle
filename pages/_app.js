e4247992634a2b8194d6102ea417cd430d3b3316
Trung Trần <trungtran@MacBook-Air-cua-Trung.local>
2023-08-21 21:59:40 +0700

    create manual header with useMoralis hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        // initializeOnMount: the optionality to hook into a web server to add some more features to our website
+        // but we want our app is completely open source and don't want any additional feature, so we will turn it off
+        <MoralisProvider initializeOnMount={false}>
+            <Component {...pageProps} />
+        </MoralisProvider>
+    );
 }
